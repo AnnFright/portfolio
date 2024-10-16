03c1355ac72fc9af0826544c25b9fb508ec7cb63
AnnFright <tea1sir@gmail.com>
2024-10-16 21:31:26 +0300

    исправление js 2, очистка index

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
 'use strict';
-const data = [
-    {skill:'html',level:60,class:'skill-item_html',iconName:'Icon1HTML.svg'},
-    {skill:'css',level:55,class:'skill-item_css',iconName:'Icon2CSS.svg'},
-    {skill:'python',level:30,class:'skill-item_python',iconName:'Icon3Py.svg'},
-    {skill:'cpp',level:40,class:'skill-item_cpp',iconName:'Icon4CPP.svg'},
-    {skill:'english',level:40,class:'skill-item_english',iconName:'Icon5English.svg'},
+let skills = {
+    data : [
+        {skill:'html',level:60,class:'skill-item_html',iconName:'Icon1HTML.svg'},
+        {skill:'css',level:55,class:'skill-item_css',iconName:'Icon2CSS.svg'},
+        {skill:'python',level:30,class:'skill-item_python',iconName:'Icon3Py.svg'},
+        {skill:'cpp',level:40,class:'skill-item_cpp',iconName:'Icon4CPP.svg'},
+        {skill:'english',level:44,class:'skill-item_english',iconName:'Icon5English.svg'},
+    ],
+    generateList(parentElement){
+        this.data.forEach(element => {
+            let dt = document.createElement('dt')
+            dt.classList.add(element.class, 'skill-item')
+            dt.textContent=element.skill
+            dt.style.backgroundImage=`url('./img/${element.iconName}')`
 
-]
+            let dd = document.createElement('dd')
+            dd.classList.add('skill-level')
 
-let skillList = document.querySelector('dl.skill-list');
+            let div = document.createElement('div')
+            div.textContent=`${element.level}%`
+            div.style.width=div.textContent
 
-data.forEach(element => {
-    let dt = document.createElement('dt');
-    dt.classList.add('skill-item', element.class);
-    dt.textContent = element.item;
-    dt.style.backgroundImage = `url('./img/${element.iconName}')`
+            dd.appendChild(div)
+            parentElement.appendChild(dt)
+            parentElement.appendChild(dd)
+        });
+        return parentElement
+    }
+}
 
-    let dd = document.createElement('dd');
-    dd.classList.add('skill-level');
-            
-    let div = document.createElement('div');
-    div.textContent = `${element.level}%`;
-    div.style.width = `${element.level}%`;
-
-    dd.appendChild(div);
-    skillList.appendChild(dt);
-    skillList.appendChild(dd);
-});
+skills.generateList(document.querySelector('dl.skill-list'))
