6521c68d8f639a91910fb938643de6ba1aaf16cd
AnnFright <tea1sir@gmail.com>
2024-11-27 21:29:45 +0300

    Новая версия

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ let skills = {
         {skill:'c++', level:40, iconName:'Icon4CPP.svg'},
         {skill:'english', level:44, iconName:'Icon5English.svg'},
     ],
-    sortMode: null,
-    generateList(parentElement){ 
+    isSorted: false,
+    generateList(parentElement){
         parentElement.innerHTML = '';
         this.data.forEach(element => {
             let dt = document.createElement('dt');
@@ -27,88 +27,60 @@ let skills = {
             parentElement.append(dt,dd);
         });
     },
-
-    sortList(type) {
-        if(this.sortMode!=type) {
-            this.data.sort(this.getComparer(type));
-            console.log(`Отсортировали данные по ${type}`);
-        }
-        else{
+    sortList: function(sortingType) {
+        if (skills.isSorted !== sortingType){
+            switch (sortingType){
+                case 'name':
+                    this.data.sort((a,b) => a.skill.localeCompare(b.skill));
+                    console.log(`Отсортировали данные по ${sortingType}`);
+                    break;
+                    
+                case 'level':
+                    this.data.sort((a,b) => b.level - a.level); 
+                    console.log(`Отсортировали данные по ${sortingType}`);
+                    break;
+                default:
+                    return;
+            }
+            this.isSorted = sortingType;
+        } else {
             this.data.reverse();
             console.log(`Инвертировали порядок сортировки`);
         }
-        this.sortMode = type;
-      },
-
-    getComparer(prop) {
-        return function(a, b) {
-          if (a[prop] < b[prop]) {
-              return -1;
-          }
-          if (a[prop] > b[prop]) {
-              return 1;
-          }
-          return 0; 
-        }
+        this.generateList(document.querySelector("dl.skill-list"));
     }
-};
-
-    const skillList = document.querySelector('dl.skill-list');
-    const sortBtnsBlock = document.querySelector('.skills-sort');
-    const menu = {
-        nav: null,
-        btn: null,
-
-        init(navSelector, btnSelector) {
-            this.nav = document.querySelector(navSelector);
-            this.btn = document.querySelector(btnSelector);
-            this.close();
-            this.btn.addEventListener('click', this.toggle.bind(this));
-        },
-
-        open() {
-            this.nav.classList.remove('main-nav_closed');
-            this.btn.classList.remove('button-nav_open');
-            this.btn.classList.add('button-nav_close');
-            this.btn.innerHTML = '<span class="visually-hidden"> Закрыть меню</span>';
-        },
-
-        close() {
-            this.nav.classList.add('main-nav_closed');
-            this.btn.classList.remove('button-nav_close');
-            this.btn.classList.add('button-nav_open');
-            this.btn.innerHTML = '<span class="visually-hidden"> Открыть меню</span>';
-        },
-
-        toggle() {
-            if (this.nav.classList.contains('main-nav_closed')) {
-            this.open();
-            } else {
-            this.close();
-            }
-        }
-};
-
-skills.generateList(skillList);
-
-menu.init('.main-nav', '.button-nav');
-sortBtnsBlock.addEventListener('click', handleButtonClick);
-
-function handleButtonClick(event) {
-  if (event.target.nodeName === 'BUTTON') {
-    switch (event.target.dataset.type) {
-      case 'name':
-        skills.sortList('name');
-        skills.generateList(skillList);
-        break;
-      case 'level':
-        skills.sortList('level');
-        skills.generateList(skillList);
-        break;
-      default:
-        console.log('Неизвестная ошибка');
-    }
-  }
 }
 
-menu.open();
+skills.generateList(document.querySelector('dl.skill-list'));
+
+const sortBtnBlock = document.querySelector("div.skills-sort");
+sortBtnBlock.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target.nodeName === "BUTTON")
+    {
+        skills.sortList(target.dataset.type);
+    } 
+});
+
+const nav = document.querySelector('.main-nav');
+const btn = document.querySelector('.button-nav');
+
+const menu = {
+    open: function() {
+        nav.classList.remove('main-nav_closed');
+        btn.classList.add('button-nav_close');
+        btn.classList.remove('button-nav_open');
+        btn.innerHTML = '<span class="visually-hidden">Закрыть меню</span>';
+    },
+    close: function() {
+        nav.classList.add('main-nav_closed');
+        btn.classList.remove('button-nav_close');
+        btn.classList.add('button-nav_open');
+        btn.innerHTML = '<span class="visually-hidden">Открыть меню</span>';
+    },
+};
+
+btn.addEventListener('click', (e) => {
+    e.target.classList.contains('button-nav_open') ? menu.open() : menu.close();
+});
+menu.close();
